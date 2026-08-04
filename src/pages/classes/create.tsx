f782b6361652fb7112ebb9be61043faa27f2a3d3
Tuan Phan <tuanpv29@gmail.com>
2026-08-04 23:50:33 +0700

    finalize create classes form

--- src/pages/classes/create.tsx
+++ src/pages/classes/create.tsx
@@ -1,7 +1,7 @@
 import { CreateView } from "@/components/refine-ui/views/create-view.tsx";
 import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb.tsx";
 import { Button } from "@/components/ui/button.tsx";
-import { useBack } from "@refinedev/core";
+import { useBack, useList } from "@refinedev/core";
 import { Separator } from "@/components/ui/separator.tsx";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card.tsx"
 import { zodResolver } from "@hookform/resolvers/zod"
@@ -17,12 +17,12 @@ import {
     FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label.tsx";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select.tsx";
 import { Textarea } from "@/components/ui/textarea.tsx";
 import { Loader2 } from "lucide-react";
 import UploadWidget from "@/components/upload-widget";
 import { useWatch } from "react-hook-form";
+import { Subject, User } from "@/types";
 
 
 const Create = () => {
@@ -37,6 +37,7 @@ const Create = () => {
     });
 
     const {
+        refineCore: { onFinish },
         handleSubmit,
         formState: { isSubmitting, errors },
         control,
@@ -44,35 +45,38 @@ const Create = () => {
 
     const onSubmit = async (values: z.infer<typeof classSchema>) => {
         try {
-            console.log(values);
+            await onFinish(values);
         } catch (error) {
             console.error("Error creating class:", error);
         }
     };
 
-    const teachers = [
-        {
-            id: 1,
-            name: "John Doe",
+    const { query: subjectsQuery } = useList<Subject>({
+        resource: "subjects",
+        pagination: {
+            pageSize: 100,
         },
-        {
-            id: 2,
-            name: "Jane Doe",
-        },
-    ];
+    });
 
-    const subjects = [
-        {
-            id: 1,
-            name: "Math",
-            code: "MATH",
+    const { query: teachersQuery } = useList<User>({
+        resource: "users",
+        filters: [
+            {
+                field: "role",
+                operator: "eq",
+                value: "teacher",
+            },
+        ],
+        pagination: {
+            pageSize: 100,
         },
-        {
-            id: 2,
-            name: "English",
-            code: "ENG",
-        },
-    ];
+    });
+
+    const subjects = subjectsQuery?.data?.data || [];
+    const subjectsLoading = subjectsQuery?.isLoading;
+
+    const teachers = teachersQuery?.data?.data || [];
+    const teachersLoading = teachersQuery?.isLoading;
 
     const bannerPublicId = form.watch("bannerCldPubId");
 
@@ -120,7 +124,7 @@ const Create = () => {
                                             <FormControl>
                                                 <UploadWidget
                                                     value={field.value ? { url: field.value, publicId: bannerPublicId ?? "" } : null}
-                                                    onChange={(file: any, field: any) => setBannerImage(file, field)} />
+                                                    onChange={(file: any) => setBannerImage(file, field)} />
                                             </FormControl>
                                             <FormMessage />
                                             {errors.bannerUrl && !errors.bannerUrl && <p className="text-destructive text-sm">{errors.bannerCldPubId?.message?.toString()}</p>}
@@ -161,6 +165,7 @@ const Create = () => {
                                                         field.onChange(Number(value))
                                                     }
                                                     value={field.value?.toString()}
+                                                    disabled={subjectsLoading}
                                                 >
                                                     <FormControl>
                                                         <SelectTrigger className="w-full">
@@ -194,6 +199,7 @@ const Create = () => {
                                                 <Select
                                                     onValueChange={field.onChange}
                                                     value={field.value}
+                                                    disabled={teachersLoading}
                                                 >
                                                     <FormControl>
                                                         <SelectTrigger className="w-full">
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,5 @@
 import { BACKEND_BASE_URL } from "@/constants";
-import { ListResponse } from "@/types";
+import { CreateResponse, ListResponse } from "@/types";
 import { HttpError } from "@refinedev/core";
 import { CreateDataProviderOptions, createDataProvider } from "@refinedev/rest";
 
@@ -58,6 +58,18 @@ const options: CreateDataProviderOptions = {
       const payload: ListResponse = await response.json();
 
       return payload.pagination?.total ?? payload.data?.length ?? 0;
+    }
+  },
+
+  create: {
+    getEndpoint: ({ resource }) => resource,
+
+    buildBodyParams: async ({ variables }) => variables,
+
+    mapResponse: async (response) => {
+      const json: CreateResponse = await response.json();
+
+      return json.data ?? [];
     }
   }
 }
